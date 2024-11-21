19d8d16ffe9608e85b6480922d19220535955933
Miroslav <94078303+miRx923@users.noreply.github.com>
2024-11-21 15:23:00 +0100

    Changed model initialization a bit

--- models/store_sales_gru_model.py
+++ models/store_sales_gru_model.py
@@ -20,11 +20,12 @@ class StoreSalesGRUModel(BaseModel):
 
         super(StoreSalesGRUModel, self).__init__(model_properties)
         
-        self.input_size = model_properties["input_size"]
-        self.hidden_size = model_properties["hidden_size"]
-        self.num_layers = model_properties["num_layers"]
-        self.output_size = model_properties["output_size"]
-        self.dropout = model_properties["dropout"]
+        # Initializing with default values if not provided in model_properties
+        self.input_size = model_properties.get(key="input_size", default=15)    # Default: 15 input features
+        self.hidden_size = model_properties.get(key="hidden_size", default=64)  # Default: 64 hidden neurons
+        self.num_layers = model_properties.get(key="num_layers", default=2)     # Default: 2 GRU layers
+        self.output_size = model_properties.get(key="output_size", default=15)  # Default: prediction for 15 days
+        self.dropout = model_properties.get(key="dropout", default=0.0)         # Default: no dropout
         
         self.init_model()
 
@@ -35,15 +36,15 @@ class StoreSalesGRUModel(BaseModel):
         """
     
         self.gru = nn.GRU(
-            self.input_size, 
-            self.hidden_size, 
-            self.num_layers, 
-            batch_first=True, 
-            dropout=self.dropout if self.num_layers > 1 else 0  # Dropout only if num_layers > 1
+            input_size = self.input_size, 
+            hidden_size = self.hidden_size, 
+            num_layers = self.num_layers, 
+            batch_first = True, 
+            dropout = self.dropout if self.num_layers > 1 else 0.0  # Dropout only if num_layers > 1
         )
         
-        self.fc = nn.Linear(self.hidden_size, self.output_size)
-        self.relu = nn.ReLU()
+        self.fc = nn.Linear(in_features = self.hidden_size, out_features = self.output_size)
+        self.relu = nn.ReLU(inplace = True)
 
 
     def forward(self, x) -> torch.Tensor:
