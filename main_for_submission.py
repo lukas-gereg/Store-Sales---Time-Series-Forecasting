14991fb05ef3f963063e5892c7c64956a38d0934
Miroslav <94078303+miRx923@users.noreply.github.com>
2024-12-01 17:14:09 +0100

    nepamatam si co som menil a preco

--- main_for_submission.py
+++ main_for_submission.py
@@ -8,31 +8,67 @@ from sklearn.metrics import mean_squared_log_error
 from models.store_sales_gru_model import StoreSalesGRUModel
 from Data.sales_dataset import SalesDataset
 
+
+
+# Funkcia na vytváranie časovo závislých príznakov
+def create_date_features(df):
+    df['month'] = df.date.dt.month.astype("int8")
+    df['day_of_month'] = df.date.dt.day.astype("int8")
+    df['day_of_year'] = df.date.dt.dayofyear.astype("int16")
+    df['week_of_month'] = (df.date.apply(lambda d: (d.day - 1) // 7 + 1)).astype("int8")
+    df['week_of_year'] = df.date.dt.isocalendar().week.astype("int8")
+    df['day_of_week'] = (df.date.dt.dayofweek + 1).astype("int8")
+    df['year'] = df.date.dt.year.astype("int32")
+    df["is_wknd"] = (df.date.dt.weekday // 4).astype("int8")
+    df["quarter"] = df.date.dt.quarter.astype("int8")
+    df['is_month_start'] = df.date.dt.is_month_start.astype("int8")
+    df['is_month_end'] = df.date.dt.is_month_end.astype("int8")
+    df['is_quarter_start'] = df.date.dt.is_quarter_start.astype("int8")
+    df['is_quarter_end'] = df.date.dt.is_quarter_end.astype("int8")
+    df['is_year_start'] = df.date.dt.is_year_start.astype("int8")
+    df['is_year_end'] = df.date.dt.is_year_end.astype("int8")
+    df["season"] = np.where(df.month.isin([12, 1, 2]), 0, 1)
+    df["season"] = np.where(df.month.isin([6, 7, 8]), 2, df["season"])
+    df["season"] = pd.Series(np.where(df.month.isin([9, 10, 11]), 3, df["season"])).astype("int8")
+    return df
+
+
+
 def main():
     # Load and preprocess training dataset
     train_data_path = './MyData/final_train_dataset.csv'
     df = pd.read_csv(train_data_path)
 
     # # Preprocess the dataset
-    df['date'] = pd.to_datetime(df['date'])
-    df['days_from_start'] = (df['date'] - df['date'].min()).dt.days
-    df = df.drop(columns=['date'])
+    # df['date'] = pd.to_datetime(df['date'])
+    # df['days_from_start'] = (df['date'] - df['date'].min()).dt.days
+    # df = df.drop(columns=['date'])
 
     # Prekonvertovanie stĺpca 'date' na datetime formát
-    #df['date'] = pd.to_datetime(df['date'])
+    df['date'] = pd.to_datetime(df['date'])
 
-    # Filtrovanie dát len pre roky 2016 a 2017
-    #df = df[(df['date'].dt.year >= 2016) & (df['date'].dt.year <= 2017)]
+    # Pridanie časovo závislých príznakov
+    df = create_date_features(df)
 
-    # Vytvorenie stĺpca 'days_from_start', ktorý počíta dni od najstaršieho dátumu
-    #df['days_from_start'] = (df['date'] - df['date'].min()).dt.days
+    #Filtrovanie dát len pre roky 2016 a 2017
+    df = df[(df['date'].dt.year >= 2016) & (df['date'].dt.year <= 2017)]
 
-    # Odstránenie stĺpca 'date', ak už nie je potrebný
-    #df = df.drop(columns=['date'])
+    #Vytvorenie stĺpca 'days_from_start', ktorý počíta dni od najstaršieho dátumu
+    df['days_from_start'] = (df['date'] - df['date'].min()).dt.days
+
+    # Pridanie príznakov na základe pracovných dní a výplatných dní
+    df["workday"] = np.where(
+        (df['day_of_week'].isin([6, 7])) | (df['is_month_end'] == 1) | (df['day_of_month'] == 15),
+        0, 1
+    ).astype("int8")
+    df["wageday"] = np.where((df['is_month_end'] == 1) | (df['day_of_month'] == 15), 1, 0).astype("int8")
+
+    #Odstránenie stĺpca 'date', ak už nie je potrebný
+    df = df.drop(columns=['date'])
 
     # Define sequence length and split into train/validation sets
-    sequence_length = 100
-    val_size = 0.1  # 10% of the data for validation
+    sequence_length = 30
+    val_size = 0.05  # 10% of the data for validation
     val_split_size = int(len(df) * val_size)
 
     train_df = df.iloc[:-val_split_size]
@@ -42,17 +78,17 @@ def main():
     train_dataset = SalesDataset(train_df, sequence_length=sequence_length)
     val_dataset = SalesDataset(val_df, sequence_length=sequence_length)
 
-    batch_size = 1024
+    batch_size = 2048
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=6, pin_memory=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=6, pin_memory=True)
 
     # Define network parameters
     model_properties = {
         'input_size': train_df.drop(columns=['sales']).shape[1],  # Number of input features
-        'hidden_size': 128,
-        'num_layers': 4,
+        'hidden_size': 32,
+        'num_layers': 2,
         'output_size': 1,  # Predicting one "sales" value
-        'dropout': 0.1
+        'dropout': 0.2
     }
 
     # Initialize the model
@@ -63,12 +99,12 @@ def main():
     model.to(device)
 
     # Define optimizer and loss function
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.0001)
     criterion = nn.MSELoss()
 
     # Early stopping parameters
-    num_epochs = 50
-    patience = 15  # Stop training if validation loss doesn't improve for 15 consecutive epochs
+    num_epochs = 25
+    patience = 5  # Stop training if validation loss doesn't improve for 15 consecutive epochs
     best_val_loss = float('inf')
     trigger_times = 0
 
